aa497cbc0351380274816b966125305d81fdbffb
Sergei <sergei199@list.ru>
2021-07-06 20:36:03 +0300

    Переименована ветка Task2 на Task2.2 SoftAssertions вынесены в отдельный метод Добавлен remote web driver

--- src/main/java/eu/senla/shabalin/DataFixture.java
+++ src/main/java/eu/senla/shabalin/DataFixture.java
@@ -1,5 +1,6 @@
 package eu.senla.shabalin;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,7 +15,8 @@ public class DataFixture {
 
     @BeforeAll
     public static void beforeAllTest() {
-        System.setProperty("webdriver.chrome.driver", "src/main/java/eu/senla/shabalin/driver/chromedriver91.0.4472.101");
+//        System.setProperty("webdriver.chrome.driver", "src/main/java/eu/senla/shabalin/driver/chromedriver91.0.4472.101");
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--window-size=1920,1200", "--no-sandbox");
         driver = new ChromeDriver(options);
--- src/test/java/AllTest.java
+++ src/test/java/AllTest.java
@@ -5,41 +5,41 @@ import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 public class AllTest extends DataFixture {
-    private String correctLogin = "tomsmith";
-    private String incorrectLogin = "tomsmith123";
-    private String correctPassword = "SuperSecretPassword!";
-    private String incorrectPassword = "NotSuperSecretPassword!";
-    private String loginPageUrl = "http://the-internet.herokuapp.com/login";
-    private String mainPageUrl = "http://the-internet.herokuapp.com/secure";
-    SoftAssertions assertions = new SoftAssertions();
+    private final String correctLogin = "tomsmith";
+    private final String incorrectLogin = "tomsmith123";
+    private final String correctPassword = "SuperSecretPassword!";
+    private final String incorrectPassword = "NotSuperSecretPassword!";
+    private final String loginPageUrl = "http://the-internet.herokuapp.com/login";
+    private final String mainPageUrl = "http://the-internet.herokuapp.com/secure";
+    private SoftAssertions assertions = new SoftAssertions();
     private LoginPage loginPage;
     private MainPage mainPage;
+
+    private void assertUrlAndAlertMessage(String pageUrl, String alertMessage) {
+        assertions.assertThat(driver.getCurrentUrl()).as("check url").isEqualTo(pageUrl);
+        assertions.assertThat(loginPage.getAlertMessage()).as("check message").isEqualTo(alertMessage);
+        assertions.assertAll();
+    }
 
     @Test
     public void loginWithIncorrectLoginAndIncorrectPassword() {
         loginPage = new LoginPage(driver);
         loginPage.loginWithUserCredentials(incorrectLogin, incorrectPassword);
-        assertions.assertThat(driver.getCurrentUrl()).as("check url").isEqualTo(loginPageUrl);
-        assertions.assertThat(loginPage.getAlertMessage()).as("check message").isEqualTo("Your username is invalid!\n×");
-        assertions.assertAll();
+        assertUrlAndAlertMessage(loginPageUrl, "Your username is invalid!\n×");
     }
 
     @Test
     public void loginWithCorrectLoginAndIncorrectPassword() {
         loginPage = new LoginPage(driver);
         loginPage.loginWithUserCredentials(correctLogin, incorrectPassword);
-        assertions.assertThat(driver.getCurrentUrl()).as("check url").isEqualTo(loginPageUrl);
-        assertions.assertThat(loginPage.getAlertMessage()).as("check message").isEqualTo("Your password is invalid!\n×");
-        assertions.assertAll();
+        assertUrlAndAlertMessage(loginPageUrl, "Your password is invalid!\n×");
     }
 
     @Test
     public void loginWithCorrectLoginAndCorrectPassword() {
         loginPage = new LoginPage(driver);
         mainPage = (MainPage) loginPage.loginWithUserCredentials(correctLogin, correctPassword);
-        assertions.assertThat(driver.getCurrentUrl()).as("check url").isEqualTo(mainPageUrl);
-        assertions.assertThat(mainPage.getAlertMessage()).as("check message").isEqualTo("You logged into a secure area!\n×");
-        assertions.assertAll();
+        assertUrlAndAlertMessage(mainPageUrl, "You logged into a secure area!\n×");
     }
 
     @Test
@@ -47,8 +47,6 @@ public class AllTest extends DataFixture {
         loginPage = new LoginPage(driver);
         mainPage = (MainPage) loginPage.loginWithUserCredentials(correctLogin, correctPassword);
         loginPage = (LoginPage) mainPage.logoutFromPage();
-        assertions.assertThat(driver.getCurrentUrl()).as("check url").isEqualTo(loginPageUrl);
-        assertions.assertThat(loginPage.getAlertMessage()).as("check message").isEqualTo("You logged out of the secure area!\n×");
-        assertions.assertAll();
+        assertUrlAndAlertMessage(loginPageUrl, "You logged out of the secure area!\n×");
     }
 }
