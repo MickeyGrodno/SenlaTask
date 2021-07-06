09f259083dbe0a0940302a196655c9911adee4b6
Siarhei Shabalin <siarhei_shabalin@senla.eu>
2021-07-06 17:59:18 +0300

    Сделано Task1 с использованием Page Factory

--- src/main/java/eu/senla/shabalin/pageobjects/LoginPage.java
+++ src/main/java/eu/senla/shabalin/pageobjects/LoginPage.java
@@ -3,25 +3,31 @@ package eu.senla.shabalin.pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
 public class LoginPage implements PageObject{
     private WebDriver driver;
-    private By usernameField = By.id("username");
-    private By passwordField = By.id("password");
-    private By loginButton = By.className("radius");
-    private By alertMessage = By.id("flash");
+    @FindBy(id = "username")
+    private WebElement usernameField;
+    @FindBy(id = "password")
+    private WebElement passwordField;
+    @FindBy(className = "radius")
+    private WebElement loginButton;
+    @FindBy(id = "flash")
     private List<WebElement> elements;
 
     public LoginPage (WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public PageObject loginWithUserCredentials(String userName, String userPassword) {
-        driver.findElement(usernameField).sendKeys(userName);
-        driver.findElement(passwordField).sendKeys(userPassword);
-        driver.findElement(loginButton).click();
+        usernameField.sendKeys(userName);
+        passwordField.sendKeys(userPassword);
+        loginButton.click();
 
         if (driver.getCurrentUrl().equals("http://the-internet.herokuapp.com/login")) {
             return this;
@@ -31,7 +37,6 @@ public class LoginPage implements PageObject{
     }
 
     public String getAlertMessage() {
-        elements = driver.findElements(alertMessage);
         if(elements.size() == 1) {
             return elements.get(0).getText();
         } else {
--- src/main/java/eu/senla/shabalin/pageobjects/MainPage.java
+++ src/main/java/eu/senla/shabalin/pageobjects/MainPage.java
@@ -3,28 +3,31 @@ package eu.senla.shabalin.pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
 public class MainPage implements PageObject{
     private WebDriver driver;
-    private By logoutButton = By.className("icon-signout");
-    private By alertMessage = By.id("flash");
-    private List<WebElement> elements;
+    @FindBy(className = "icon-signout")
+    private WebElement logoutButton;
+    @FindBy(id = "flash")
+    private List<WebElement> alerts;
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public PageObject logoutFromPage() {
-        driver.findElement(logoutButton).click();
+        logoutButton.click();
         return new LoginPage(driver);
     }
 
     public String getAlertMessage() {
-        elements = driver.findElements(alertMessage);
-        if(elements.size() == 1) {
-            return elements.get(0).getText();
+        if(alerts.size() == 1) {
+            return alerts.get(0).getText();
         } else {
             return null;
         }
