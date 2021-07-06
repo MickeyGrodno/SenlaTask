a95954bd4ddd575465c06e914511b0c321825f7e
Sergei <sergei199@list.ru>
2021-07-06 20:36:03 +0300

    Переименована ветка Task2 на Task2.2 SoftAssertions вынесены в отдельный метод Добавлен remote web driver

---
+++ src/test/java/AllTest.java
@@ -0,0 +1,52 @@
+import eu.senla.shabalin.DataFixture;
+import eu.senla.shabalin.pageobjects.LoginPage;
+import eu.senla.shabalin.pageobjects.MainPage;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+public class AllTest extends DataFixture {
+    private final String correctLogin = "tomsmith";
+    private final String incorrectLogin = "tomsmith123";
+    private final String correctPassword = "SuperSecretPassword!";
+    private final String incorrectPassword = "NotSuperSecretPassword!";
+    private final String loginPageUrl = "http://the-internet.herokuapp.com/login";
+    private final String mainPageUrl = "http://the-internet.herokuapp.com/secure";
+    private SoftAssertions assertions = new SoftAssertions();
+    private LoginPage loginPage;
+    private MainPage mainPage;
+
+    private void assertUrlAndAlertMessage(String pageUrl, String alertMessage) {
+        assertions.assertThat(driver.getCurrentUrl()).as("check url").isEqualTo(pageUrl);
+        assertions.assertThat(loginPage.getAlertMessage()).as("check message").isEqualTo(alertMessage);
+        assertions.assertAll();
+    }
+
+    @Test
+    public void loginWithIncorrectLoginAndIncorrectPassword() {
+        loginPage = new LoginPage(driver);
+        loginPage.loginWithUserCredentials(incorrectLogin, incorrectPassword);
+        assertUrlAndAlertMessage(loginPageUrl, "Your username is invalid!\n×");
+    }
+
+    @Test
+    public void loginWithCorrectLoginAndIncorrectPassword() {
+        loginPage = new LoginPage(driver);
+        loginPage.loginWithUserCredentials(correctLogin, incorrectPassword);
+        assertUrlAndAlertMessage(loginPageUrl, "Your password is invalid!\n×");
+    }
+
+    @Test
+    public void loginWithCorrectLoginAndCorrectPassword() {
+        loginPage = new LoginPage(driver);
+        mainPage = (MainPage) loginPage.loginWithUserCredentials(correctLogin, correctPassword);
+        assertUrlAndAlertMessage(mainPageUrl, "You logged into a secure area!\n×");
+    }
+
+    @Test
+    public void SuccessLogoutFromMainPage() {
+        loginPage = new LoginPage(driver);
+        mainPage = (MainPage) loginPage.loginWithUserCredentials(correctLogin, correctPassword);
+        loginPage = (LoginPage) mainPage.logoutFromPage();
+        assertUrlAndAlertMessage(loginPageUrl, "You logged out of the secure area!\n×");
+    }
+}
