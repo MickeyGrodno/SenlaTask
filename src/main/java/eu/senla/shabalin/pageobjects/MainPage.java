a0c178b862eb99b5e84710ab1a99cbefb06463e3
Siarhei Shabalin <siarhei_shabalin@senla.eu>
2021-07-07 09:22:47 +0300

    Изменено содержание README на задние Task2

--- src/main/java/eu/senla/shabalin/pageobjects/MainPage.java
+++ src/main/java/eu/senla/shabalin/pageobjects/MainPage.java
@@ -3,31 +3,28 @@ package eu.senla.shabalin.pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
 public class MainPage implements PageObject{
     private WebDriver driver;
-    @FindBy(className = "icon-signout")
-    private WebElement logoutButton;
-    @FindBy(id = "flash")
-    private List<WebElement> alerts;
+    private final By LOGOUT_BUTTON = By.className("icon-signout");
+    private final By ALERT_MESSAGE = By.id("flash");
+    private List<WebElement> elements;
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, this);
     }
 
     public PageObject logoutFromPage() {
-        logoutButton.click();
+        driver.findElement(LOGOUT_BUTTON).click();
         return new LoginPage(driver);
     }
 
     public String getAlertMessage() {
-        if(alerts.size() == 1) {
-            return alerts.get(0).getText();
+        elements = driver.findElements(ALERT_MESSAGE);
+        if(elements.size() == 1) {
+            return elements.get(0).getText();
         } else {
             return null;
         }
