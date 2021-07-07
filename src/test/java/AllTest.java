129aa81c66bc6560d65e9f9aad034150192426a6
Siarhei Shabalin <siarhei_shabalin@senla.eu>
2021-07-07 13:17:52 +0300

    Реализован тест проверки наличия динамического контента на странице.

--- src/test/java/AllTest.java
+++ src/test/java/AllTest.java
@@ -1,15 +1,14 @@
 import eu.senla.shabalin.DataFixture;
 import eu.senla.shabalin.pageobjects.DynamicContent;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class AllTest extends DataFixture {
 
     @Test
-    public void first() throws InterruptedException {
+    public void dynamicContentIsPresentInPage() {
         driver.get(property.getProperty("dynamic.content.url"));
         DynamicContent content = new DynamicContent(driver);
-        Assertions.assertTrue();
-}
+        assertTrue(content.isDynamicContentPresent());
+    }
 }
