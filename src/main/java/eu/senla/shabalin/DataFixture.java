42d13c94f324de12db7d3b95767cdcfb3e4172fd
Siarhei Shabalin <siarhei_shabalin@senla.eu>
2021-07-14 12:22:08 +0300

    Добавлен автовыбор стартового Url в зависимости от запускаемого тестового набора

--- src/main/java/eu/senla/shabalin/DataFixture.java
+++ src/main/java/eu/senla/shabalin/DataFixture.java
@@ -1,7 +1,6 @@
 package eu.senla.shabalin;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,17 +9,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
-
 
 public class DataFixture {
     protected static WebDriver driver;
     protected static Properties property;
     protected static ChromeOptions options;
     protected static String loginPageUrl;
+    protected static String dynamicControlsUrl;
+    protected static String dynamicContentUrl;
+
 
     @BeforeAll
     public static void beforeAllTest() throws IOException {
@@ -28,9 +27,6 @@ public class DataFixture {
         options = new ChromeOptions();
         options.addArguments("--window-size=1920,1200", "--no-sandbox");
         options.setHeadless(true);
-        property = new Properties();
-        property.load(new FileInputStream("src/main/resources/config.properties"));
-        loginPageUrl = property.getProperty("loginPageUrl");
         String pathToThePropertyFile;
         try {
             if(System.getProperty("os.name").equals("Linux")) {
@@ -38,17 +34,28 @@ public class DataFixture {
             } else {
                 pathToThePropertyFile = "src\\main\\resources\\config.properties";
             }
-            FileInputStream fis = new FileInputStream(pathToThePropertyFile);
-            property.load(fis);
+            property = new Properties();
+            property.load(new FileInputStream(pathToThePropertyFile));
         } catch (IOException e) {
             System.err.println("Property file don't found!");
         }
+        loginPageUrl = property.getProperty("loginPageUrl");
+        dynamicControlsUrl = property.getProperty("dynamicControlsUrl");
+        dynamicContentUrl = property.getProperty("dynamicContentUrl");
     }
     @BeforeEach
     public void beforeTest() {
         driver = new ChromeDriver(options);
         if(this.getClass().toString().equals("class eu.senla.shabalin.LoginPageTest")) {
             driver.get(loginPageUrl);
+        } else {
+            if (this.getClass().toString().equals("class eu.senla.shabalin.DynamicControlsTest")) {
+                driver.get(dynamicControlsUrl);
+            } else {
+                if(this.getClass().toString().equals("class eu.senla.shabalin.DynamicContentTest")) {
+                    driver.get(dynamicContentUrl);
+                }
+            }
         }
     }
 
