8fd092418023a08606d434d8583fa04cf33f6d2f
Sergei <sergei199@list.ru>
2021-07-17 20:26:55 +0300

    Произведен рефакторинг кода

--- src/main/java/eu/senla/shabalin/pageobjects/ComputerDatabasePage.java
+++ src/main/java/eu/senla/shabalin/pageobjects/ComputerDatabasePage.java
@@ -6,52 +6,50 @@ import eu.senla.shabalin.enums.SortBy;
 import eu.senla.shabalin.enums.Column;
 
 import java.util.List;
+import java.util.Objects;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
 
 public class ComputerDatabasePage {
-    private String startPageUrl = "https://computer-database.gatling.io/computers";
-    private String headerSortUp = "headerSortUp";
-    private String headerSortDown = "headerSortDown";
-    private SelenideElement headComputerName = $("th.col-name");
-    private SelenideElement headComputerCompany = $("th.col-company");
-    private SelenideElement displayAllRowsCount = $("li.current");
-    private ElementsCollection computerNameElements = $$("td>a");
-    private ElementsCollection computerCompanyElements = $$("tr>td:nth-child(4)");
-    private ElementsCollection rowCollection = $$("tbody>tr");
+    private final String START_PAGE_URL = "https://computer-database.gatling.io/computers";
+    private final String HEADER_SORT_UP = "headerSortUp";
+    private final String HEADER_SORT_DOWN = "headerSortDown";
+    private final SelenideElement HEADER_COMPUTER_NAME = $("th.col-name");
+    private final SelenideElement HEAD_COMPUTER_COMPANY = $("th.col-company");
+    private final SelenideElement DISPLAY_ALL_ROWS_COUNT = $("li.current");
+    private final ElementsCollection COMPUTER_NAME_ELEMENTS = $$("td>a");
+    private final ElementsCollection COMPUTER_COMPANY_ELEMENTS = $$("tr>td:nth-child(4)");
+    private final ElementsCollection ROW_COLLECTION = $$("tbody>tr");
 
     public ElementsCollection getAllRowsFromPage() {
-        return rowCollection;
+        return ROW_COLLECTION;
     }
 
     public List<String> getComputerColumnInString(Column column) {
-        if(column == Column.NAME){
-            return computerNameElements.texts();
+        if (column.equals(Column.NAME)) {
+            return COMPUTER_NAME_ELEMENTS.texts();
         } else {
-            return computerCompanyElements.texts();
+            return COMPUTER_COMPANY_ELEMENTS.texts();
         }
     }
 
     public void openFirstPage() {
-        open(startPageUrl);
+        open(START_PAGE_URL);
     }
 
     public void openPenultimatePage() {
-        String[] text = displayAllRowsCount.getText().split(" ");
-        int penultimatePage =(Integer.parseInt(text[text.length-1])/10)-1;
-        open(url().replaceFirst( "p=[0-9]", "p="+penultimatePage));
+        String[] text = DISPLAY_ALL_ROWS_COUNT.getText().split(" ");
+        int penultimatePage = (Integer.parseInt(text[text.length - 1]) / 10) - 1;
+        open(url().replaceFirst("p=[0-9]", "p=" + penultimatePage));
     }
 
     public void sortAllBy(Column sortedColumn, SortBy sortBy) {
-        String sorter;
-        SelenideElement column;
 
-        if(sortedColumn == Column.NAME) { column = headComputerName; }
-        else { column = headComputerCompany; }
-        if(sortBy == SortBy.ASC) { sorter = headerSortUp; }
-        else { sorter = headerSortDown; }
-        while(!column.getAttribute("class").contains(sorter)) {
+        SelenideElement column = sortedColumn.equals(Column.NAME) ? HEADER_COMPUTER_NAME : HEAD_COMPUTER_COMPANY;
+        String sorter = sortBy.equals(SortBy.ASC) ? HEADER_SORT_UP : HEADER_SORT_DOWN;
+
+        while (!Objects.requireNonNull(column.getAttribute("class")).contains(sorter)) {
             column.$("a").click();
         }
     }
