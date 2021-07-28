56a519a98ee2eaf2a873b7ff109ba0ea89fa9868
Sergei <sergei199@list.ru>
2021-07-28 17:07:14 +0300

    Добавлен простой тест на сложение

--- src/test/java/eu/senla/shabalin/SimpleMathTest.java
+++ src/test/java/eu/senla/shabalin/SimpleMathTest.java
@@ -3,15 +3,33 @@ package eu.senla.shabalin;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SimpleMathTest {
     private final int firstNumber = Integer.parseInt(System.getProperty("FIRST_NUMBER"));
     private final int secondNumber = Integer.parseInt(System.getProperty("SECOND_NUMBER"));
+    private final String message = System.getProperty("MESSAGE");
+    private final String testResult = System.getProperty("TEST_RESULT");
 
     @Test
     public void sumExampleTest() {
-        System.out.println(firstNumber);
-        System.out.println(secondNumber);
         int result = firstNumber+secondNumber;
-        Assertions.assertEquals(0, result);
+        Assertions.assertTrue(result>0);
+    }
+    @Test
+    public void minusExampleTest() {
+        int result = firstNumber-secondNumber;
+        Assertions.assertTrue(result>=0);
+    }
+    @Test
+    public void wordCheckForMessage() {
+        assertEquals("message", message.toLowerCase());
+    }
+
+    @Test
+    public void choiceTestResultTest() {
+        assertEquals("successful", testResult);
     }
 }
