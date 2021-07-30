bac31756816957ec0d814aca23caa11661bdc763
Sergei <sergei199@list.ru>
2021-07-30 10:27:56 +0300

    добавлен extent

--- src/main/java/eu/senla/shabalin/Utils.java
+++ src/main/java/eu/senla/shabalin/Utils.java
@@ -6,6 +6,6 @@ import java.text.SimpleDateFormat;
 public class Utils {
     public static String getCurrentDateTime() {
         DateFormat customFormat = new SimpleDateFormat("MM_dd_yyyy_HH_mm_ss");
-
+        return null;
     }
 }
