529f8cbefe4838a7cf104c18209baab8a5a24186
Sergei <sergei199@list.ru>
2021-07-30 17:04:50 +0300

    добавлен extent+CustomTestWatcher

--- src/main/java/eu/senla/shabalin/Fixture.java
+++ src/main/java/eu/senla/shabalin/Fixture.java
@@ -2,6 +2,9 @@ package eu.senla.shabalin;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ExtentHtmlReporterConfiguration;
+import com.aventstack.extentreports.reporter.configuration.Protocol;
+import com.aventstack.extentreports.reporter.converters.ExtentHtmlReporterConverter;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -22,9 +25,11 @@ public class Fixture {
             fileDirectory.mkdir();
         }
 
+        ExtentHtmlReporterConfiguration configuration = new ExtentHtmlReporterConfiguration();
+        configuration.setProtocol(Protocol.HTTP);
 
         ExtentHtmlReporter extent = new ExtentHtmlReporter(
-                new File(System.getProperty("user.dir")+"/extent-report/extHtmlRep.html"));
+                new File(System.getProperty("user.dir")+"/extent-report/index2.html"));
         report = new ExtentReports();
         report.attachReporter(extent);
     }
